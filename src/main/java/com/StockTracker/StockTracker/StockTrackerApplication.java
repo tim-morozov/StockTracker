59382826f9434607d28b2a9cca7643dce0d65465
tim-morozov <tim.morozov@gmail.com>
2024-02-11 12:07:54 -0600

    Adding files from Spring Initializr

---
+++ src/main/java/com/StockTracker/StockTracker/StockTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.StockTracker.StockTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackerApplication.class, args);
+	}
+
+}
